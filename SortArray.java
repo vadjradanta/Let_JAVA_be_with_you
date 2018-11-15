672ee9c98f62ce53a36a696a6ac89718c8e86ebe
Vitaly Rubanov <111revun@mail.ru>
2018-11-15 22:57:44 +0300

    Добавлены комментарии, исправлены, благодаря Николаю, методы, вывод данных осуществляется корректно

--- SortArray.java
+++ SortArray.java
@@ -2,32 +2,57 @@ package Let_JAVA_be_with_you;
 
 public class SortArray {
     public static void main(String[] args) {
-        int arr[] = new int[]{18, 0, 7, 9, 10, 6, 0, 25}; //как писал - помню, как получился верный результат - не помню
+        int arr[] = new int[]{18, 0, 7, 15, 10, 6, 1, 22};
 
-        System.out.println("Число   чётных элементов = " + nmbrOfEven(arr));
-        System.out.println("Число нечётных элементов = " + (arr.length - nmbrOfEven(arr) - nmbrOfZero(arr) ) );
-        System.out.println("Количество нулей         = " + nmbrOfZero(arr));
+        System.out.println("Число   чётных элементов = " + numberOfEven(arr));
+        System.out.println("Число нечётных элементов = " + numberOfOdd(arr));
+        System.out.println("Количество нулей         = " + numberOfZero(arr));
 }
 
-    public static int nmbrOfEven(int arr[]) {
+    /**
+     * Определение чётных элементов
+     * @param arr в массиве
+     * @return количество чётных элементов
+     */
+    public static int numberOfEven(int[] arr) {
 
-        int c = 0;
+        int countEven = 0;
 
         for (int i = 0;  i < arr.length; i++) {
             if((arr[i] % 2 == 0) && (arr[i] != 0))
-            c++;
+                countEven++;
         }
-        return c;
+        return countEven;
     }
 
-    public static int nmbrOfZero(int arr[]) {
+    /**
+     * Определяем нечётные элементы
+     * @param arr в массиве
+     * @return количество нулей
+     */
+    public static int numberOfZero(int arr[]) {
 
-        int z = 0;
+        int countZero = 0;
 
         for (int i = 0;  i < arr.length; i++) {
             if (arr[i] == 0)
-            z++;
+                countZero++;
         }
-        return z;
+        return countZero;
+    }
+
+    /**
+     * Определяем чётные элементы
+     * @param arr в массиве
+     * @return количество чётных элементов
+     */
+    public static int numberOfOdd(int[] arr){
+        int countOdd = 0;
+
+        for (int i = 0;  i < arr.length; i++) {
+            if(arr[i] % 2 != 0)
+                countOdd++;
+        }
+        return countOdd;
     }
 }
