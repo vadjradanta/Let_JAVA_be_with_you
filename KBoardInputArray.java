502fee55abe5d28c5514aea81b5a606d89dec8ff
Vitaly Rubanov <111revun@mail.ru>
2018-11-16 19:19:57 +0300

    ввод с клавиатуры через консоль

--- KBoardInputArray.java
+++ KBoardInputArray.java
@@ -1,4 +1,22 @@
 package Let_JAVA_be_with_you;
 
+import java.util.Scanner;
+
 public class KBoardInputArray {
+    public static void keyBoardArray(String[] args) {
+        int [] array;
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Введите количество элементов: ");
+        int count = scanner.nextInt();
+
+        System.out.println("Введите элеметны:");
+
+        array = new int[count];
+        for (int i = 0; i < count; i++) {
+            System.out.println("Значение" + i + " =");
+            array[i] = scanner.nextInt();
+        }
+    }
 }
+
